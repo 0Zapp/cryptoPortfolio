9143e26505a2465fa57f334a77bba82c7e55eb18
Marko Nedeljkovic <marko65734@gmail.com>
2021-01-01 18:46:35 +0100

    Baza implementirana

--- cp/cp/settings.py
+++ cp/cp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'cp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio_baza',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- cp/portfolio/models.py
+++ cp/portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    address = models.TextField()
+    amount = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.adress + " transfered " + str(amount)
